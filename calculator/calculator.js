8fbd9cef564e2f0e416e1cbfca8884a85b51c476
dtamas <tamasdorkaa@gmail.com>
2020-11-26 14:09:37 +0100

    add test case: there is a spare in the 1st frame

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,5 +1,21 @@
 module.exports = (frames) => {
-    const frameValuesInOneArray = frames.reduce((acc, curv) => [...acc, ...curv]);
-    const calculatedArray = frameValuesInOneArray.reduce((acc, curv) => acc + curv);
-    return calculatedArray;
+    const frameValuesInOneArray = frames.reduce((acc, curV) => [...acc, ...curV]);
+
+    const finalScore = frameValuesInOneArray.reduce((acc, curV, index, arr) => {
+        if(isSpare(curV)) {
+          return getScoreForSpare(acc, arr, index);
+        }
+
+        return acc + curV;
+    });
+
+    return finalScore;
+}
+
+function isSpare(value) {
+    return value === '/';
+}
+
+function getScoreForSpare(acc, arr, index) {
+    return acc + (10 - arr[index - 1]) + arr[index + 1];
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -5,7 +5,13 @@ describe('calculator', function () {
         expect(calculate([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.equal(0);
     });
 
-    it('should return 1 in case the 1st strike is 1 and all the others are 0', function () {
+    it('should return 1 in case the 1st roll is 1 and all the others are 0', function () {
         expect(calculate([[1, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.equal(1);
+    });
+
+    describe('in case of spare', function () {
+        it('should return 18 in case the 1st frame is a spare and the 3rd roll is 4', function () {
+            expect(calculate([[1, '/'], [4, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.equal(18);
+        });
     });
 });
